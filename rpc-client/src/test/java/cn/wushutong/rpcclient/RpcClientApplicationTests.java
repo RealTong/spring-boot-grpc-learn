3fe876f307169d5281235ca45b35b9368f4c12c8
吴舒桐 <49032320@qq.com>
2022-05-17 11:14:52 +0800

    feat(init grpc project): init grpc project
    init grpc project

---
+++ rpc-client/src/test/java/cn/wushutong/rpcclient/RpcClientApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.wushutong.rpcclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ rpc-interface/src/test/java/cn/wushutong/rpcinterface/RpcInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.wushutong.rpcinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
